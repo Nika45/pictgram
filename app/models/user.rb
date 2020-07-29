74b84dd9fedb503324af084fbc82913a8b0567d0
Nika <kameeri@icloud.com>
2020-07-29 16:13:35 +0000

    kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true
+  validates :password, length: { in: 8..32 }
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
+  VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
+  has_secure_password
 end
